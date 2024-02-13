52e9dc6bc49c021de66e9878150f673e3a99eb40
GeSi <sdropoulos@gmail.com>
2024-02-13 20:39:18 +0200

    Added eulers number

--- src/CalculationStuffTest.java
+++ src/CalculationStuffTest.java
@@ -4,7 +4,10 @@ public class CalculationStuffTest {
     public static void main(String[] args) {
        // int remainingNumbers;
         int factorialResult = 1;
+        int factorialResult2 = 1;
         int count;
+        int count2;
+        double eResult = 0;
 
         Scanner input = new Scanner(System.in);
         System.out.println("Give whose factorial you want:");
@@ -15,5 +18,20 @@ public class CalculationStuffTest {
             count -=1;
         }
         System.out.printf("-Factorial:%d%n-Result:%d%n",factorialOf,factorialResult);
+        System.out.println("Give whose e you want:");
+        int eOf = input.nextInt();
+        count = 0;
+        while(count <= eOf){
+            count2 = count;
+            factorialResult2 = 1;
+            //finding the factorial for denominator
+            while(count2>0){
+                factorialResult2 *= count2;
+                count2 -=1;
+            }
+            eResult += 1.0/factorialResult2;
+            count += 1;
+        }
+        System.out.printf("Approximation of e: %f.10", eResult);
     }
 }
