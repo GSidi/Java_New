048a158534d145c7f0761ad2fd240e7931b8e50d
GeSi <sdropoulos@gmail.com>
2024-03-05 20:48:48 +0200

    Creation of basic computer assisted learning (currently for maths)

---
+++ ComputerAssistedTeaching/CalculusClass.java
@@ -0,0 +1,51 @@
+public class CalculusClass {
+
+    int firstNumber;
+    int secondNumber;
+    String methodToApply;
+    double result;
+
+    public CalculusClass(int firstNumber , int secondNumber, String methodToApply){
+        this.firstNumber =  firstNumber;
+        this.secondNumber = secondNumber;
+        this.methodToApply = methodToApply;
+    }
+    public int getFirstNumber() {
+        return firstNumber;
+    }
+    public void setFirstNumber(int firstNumber) {
+        this.firstNumber = firstNumber;
+    }
+    public int getSecondNumber() {
+        return secondNumber;
+    }
+    public void setSecondNumber(int secondNumber) {
+        this.secondNumber = secondNumber;
+    }
+    public String getMethodToApply() {
+        return methodToApply;
+    }
+    public void setMethodToApply(String methodToApply) {
+        this.methodToApply = methodToApply;
+    }
+
+    public double calculusResult(int firstNumber , int secondNumber, String methodToApply){
+
+        switch (methodToApply){
+            case "1":
+                //addition
+                result = (double) firstNumber + secondNumber;
+            case "2":
+                //subtraction
+                result = (double) firstNumber - secondNumber;
+            case "3":
+                //multiplication
+                result = (double) firstNumber * secondNumber;
+            case "4":
+                //division
+                result = (double) firstNumber / secondNumber;
+        }
+
+        return result;
+    }
+}
