1818e296be7dd5cea4b2db19795307939a9aab14
GeSi <sdropoulos@gmail.com>
2024-04-29 19:42:34 +0300

    Hand Strength Function : Flush Check private function created

--- GameOfPoker/PlayerHand.java
+++ GameOfPoker/PlayerHand.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.regex.*;
 public class PlayerHand {
 
     String playerName;
@@ -23,4 +24,69 @@ public class PlayerHand {
     public void setDealtHand(ArrayList<String> dealtHand) {
         this.dealtHand = dealtHand;
     }
+
+    public String handStrength (ArrayList<String> playersHand){
+        String pairCheck;
+        String threeOfaKindCheck;
+        String straightCheck;
+        String flushCheck;
+        String fullHouseCheck;
+        String fourOfaKingCheck;
+        String straightFlushCheck;
+
+        flushCheck = this.flushColour(playersHand);
+
+        return  null;
+    }
+
+    private String flushColour(ArrayList<String> playersHand){
+
+        String flushCheck = "No Flush";
+        Boolean matchFound = false;
+        int heartCount = 0;
+        int diamondCount = 0;
+        int spadeCount = 0;
+        int clubCount = 0;
+
+        Pattern heartsPattern = Pattern.compile("Hearts");
+        Pattern diamondsPattern = Pattern.compile("Diamonds");
+        Pattern spadesPattern = Pattern.compile("Spades");
+        Pattern clubsPattern = Pattern.compile("Clubs");
+
+        for (String card : playersHand){
+            Matcher heartsMatch = heartsPattern.matcher(card);
+            matchFound = heartsMatch.find();
+            if (matchFound = true){
+                heartCount +=1;
+            }
+            Matcher diamondsMatch = diamondsPattern.matcher(card);
+            matchFound = diamondsMatch.find();
+            if (matchFound = true){
+                diamondCount +=1;
+            }
+            Matcher spadesMatch = spadesPattern.matcher(card);
+            matchFound = spadesMatch.find();
+            if (matchFound = true){
+                spadeCount +=1;
+            }
+            Matcher clubsMatch = clubsPattern.matcher(card);
+            matchFound = clubsMatch.find();
+            if (matchFound = true){
+                clubCount +=1;
+            }
+        }
+
+        if (heartCount == 5){
+            flushCheck = "Hearts Flush";
+        }else if (diamondCount == 5){
+            flushCheck = "Diamonds Flush";
+        }else if (spadeCount  == 5 ){
+            flushCheck = "Spades Flush";
+        }else if (clubCount == 5){
+            flushCheck = "Clubs Flush";
+        }
+
+        return flushCheck;
+    }
+
 }
