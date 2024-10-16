3575f18a60c9c0c6402cea61172e43a4f376efd8
GeSi <sdropoulos@gmail.com>
2024-10-16 20:06:01 +0300

    all the files

--- Strings_Chars_RegEx/StringConcat.java
+++ Strings_Chars_RegEx/StringConcat.java
@@ -4,8 +4,9 @@ public class StringConcat {
 
         String s1 = "Happy";
         String s2 = "Birthday";
-        System.out.printf("s1 = %s%ns2 = %s%n%n", s1, s2);
-        System.out.printf("Result of s1.concat(s2) = %s%n", s1.concat(s2));
-        System.out.printf("s1 after concatenation = %s%n", s1);
+        System.out.printf("s1 = %s%ns2 = %s%n%n", s1, s2); //--> output -> s1 = Happy s2 = Birthday
+        System.out.printf("Result of s1.concat(s2) = %s%n", s1.concat(s2));//--> output Result of s1.concat(s2) = Happy Birthday
+        System.out.printf("s1 after concatenation = %s%n", s1);//--> s1 after concatenation =  Happy
+
     }
 }
